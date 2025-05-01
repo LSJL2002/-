3f7c12b6fdff95ede08f2f6fc4f526a013f1e674
JavohirMX <javohirmx@gmail.com>
2025-05-01 10:51:19 +0900

    reformatting

--- complexnumbers.py
+++ complexnumbers.py
@@ -1,26 +1,29 @@
 class Complex:
-    def __init__(self,a,b):
+    def __init__(self, a, b):
         self.a = a
         self.b = b
 
-    
-    def __add__(self,operand):
-        return Complex (self.a + operand.a, self.b + operand.b)
-    
+    def __add__(self, operand):
+        return Complex(self.a + operand.a, self.b + operand.b)
+
     def __sub__(self, operand):
-        return Complex (self.a - operand.a, self.b - operand.b)
-    
+        return Complex(self.a - operand.a, self.b - operand.b)
+
     def __mul__(self, operand):
-        return Complex(self.a * operand.a - self.b * operand.b, self.a * operand.b + self.b * operand.a)
-    
+        return Complex(
+            self.a * operand.a - self.b * operand.b,
+            self.a * operand.b + self.b * operand.a,
+        )
+
     def __neg__(self):
         return Complex(-self.a, -self.b)
-    
+
     def __str__(self):
         return f"{self.a} + {self.b}i" if self.b >= 0 else f"{self.a} - {abs(self.b)}i"
-    
-A = Complex(1,2)
-B = Complex(3,4)
+
+
+A = Complex(1, 2)
+B = Complex(3, 4)
 
 print(A + B)
 print(A - B)
