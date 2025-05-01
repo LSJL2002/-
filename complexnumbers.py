c0db32aa89b9240f186cf41d31e35fd6cc451dc8
JavohirMX <javohirmx@gmail.com>
2025-05-01 10:36:26 +0900

    added multiplication

--- complexnumbers.py
+++ complexnumbers.py
@@ -11,7 +11,7 @@ class Complexnumbers:
         return Complexnumbers (self.a - operand.a, self.b - operand.b)
     
     def __multiplication__(self, operand):
-        return Complexnumbers ()
+        return Complexnumbers(self.a * operand.a - self.b * operand.b, self.a * operand.b + self.b * operand.a)
     
     def __str__(self):
         return f"{self.a} + {self.b}i"
